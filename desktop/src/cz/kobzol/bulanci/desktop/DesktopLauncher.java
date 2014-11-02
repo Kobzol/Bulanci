faa4f02d7683f712addc5a2a4c5ba0e4160aa75b
Jakub Beránek <berykubik@gmail.com>
2014-11-02 09:08:35 +0100

    Přidána kostra projektu s knihovnami LibGDX a KryoNet

---
+++ desktop/src/cz/kobzol/bulanci/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package cz.kobzol.bulanci.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import cz.kobzol.bulanci.BulanciGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BulanciGame(), config);
+	}
+}
