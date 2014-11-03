7423f5cabb03582b00137645000b7e3ea0965579
Jakub Beránek <berykubik@gmail.com>
2014-11-03 19:59:32 +0100

    Přidán rozměr k rozhraní IPositionable. Vytvořena abstraktní třída Shape implementující základní nastavení pozice a rozměru.

--- core/src/cz/kobzol/bulanci/model/IPositionable.java
+++ core/src/cz/kobzol/bulanci/model/IPositionable.java
@@ -2,6 +2,8 @@ package cz.kobzol.bulanci.model;
 
 import com.badlogic.gdx.math.Vector2;
 
+import java.awt.*;
+
 /**
  * Represents objects with position.
  */
@@ -22,5 +24,17 @@ public interface IPositionable {
      * Sets the object's position.
      * @param position position
      */
-    void setPosition(Vector2 position);
+    void setPositionX(Vector2 position);
+
+    /**
+     * Set's the object's dimension.
+     * @param dimension dimension
+     */
+    void setDimension(Dimension dimension);
+
+    /**
+     * Get's the object's dimension.
+     * @return dimension of the object
+     */
+    Dimension getDimension();
 }
---
+++ core/src/cz/kobzol/bulanci/model/Shape.java
@@ -0,0 +1,45 @@
+package cz.kobzol.bulanci.model;
+
+import com.badlogic.gdx.math.Vector2;
+
+import java.awt.*;
+
+/**
+ * Represents game shape with location and dimension.
+ */
+public abstract class Shape extends GameObject implements IPositionable  {
+    private final Dimension dimension;
+    private final Vector2 position;
+
+    public Shape(int id) {
+        super(id);
+
+        this.dimension = new Dimension();
+        this.position = new Vector2(0, 0);
+    }
+
+    @Override
+    public Dimension getDimension() {
+        return this.dimension;
+    }
+
+    @Override
+    public void setDimension(Dimension dimension) {
+        this.dimension.setSize(dimension.getSize());
+    }
+
+    @Override
+    public float getPositionX() {
+        return this.position.x;
+    }
+
+    @Override
+    public float getPositionY() {
+        return this.position.y;
+    }
+
+    @Override
+    public void setPositionX(Vector2 position) {
+        this.position.set(position);
+    }
+}
