920f1a7771d35e0dde826f8e58a22ac4a411ec6a
Jakub Beránek <berykubik@gmail.com>
2014-11-05 19:40:47 +0100

    Přidána PlayerInputHandler, která reaguje na uživatelův vstup a generuje odpovídající příkazy.

---
+++ core/src/cz/kobzol/bulanci/input/PlayerInputHandler.java
@@ -0,0 +1,44 @@
+package cz.kobzol.bulanci.input;
+
+import com.badlogic.gdx.Input;
+import cz.kobzol.bulanci.command.CommandFactory;
+import cz.kobzol.bulanci.command.ICommandInvoker;
+import cz.kobzol.bulanci.command.RotateCommand;
+
+/**
+ * Handles player's input.
+ */
+public class PlayerInputHandler {
+    private final CommandFactory commandFactory;
+    private final ICommandInvoker commandInvoker;
+
+    public PlayerInputHandler(CommandFactory commandFactory, ICommandInvoker commandInvoker) {
+        this.commandFactory = commandFactory;
+        this.commandInvoker = commandInvoker;
+    }
+
+    /**
+     * Checks the input and generates actions according to it.
+     * @param input input
+     */
+    public void checkInput(Input input) {
+        if (input.isKeyPressed(Input.Keys.RIGHT)) {
+            this.generateRotation(true);
+        }
+        else if (input.isKeyPressed(Input.Keys.LEFT)) {
+            this.generateRotation(false);
+        }
+    }
+
+    private void generateRotation(boolean rotateRight) {
+        RotateCommand.Serializable serializedCommand = new RotateCommand.Serializable(rotateRight);
+
+        try {
+            RotateCommand command = (RotateCommand) this.commandFactory.build(serializedCommand);
+            this.commandInvoker.invokeCommand(command);
+        }
+        catch (CommandFactory.UnknownCommand unknownCommandException) {
+            unknownCommandException.printStackTrace();
+        }
+    }
+}
