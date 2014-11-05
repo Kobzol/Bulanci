dea01206878e6c3f8cd60ae6fcb469460e749a7e
Jakub Beránek <berykubik@gmail.com>
2014-11-05 17:01:41 +0100

    Optimalizace vykreslování.

--- core/src/cz/kobzol/bulanci/model/SpriteObject.java
+++ core/src/cz/kobzol/bulanci/model/SpriteObject.java
@@ -18,6 +18,8 @@ public class SpriteObject extends DrawableShape implements IMovable, IRotable, I
     private Vector2 direction;
     private float speed;
 
+    private boolean dirty = true;
+
     public SpriteObject() {
         this.sprite = new Sprite();
         this.direction = new Vector2(0, -1);
@@ -30,11 +32,17 @@ public class SpriteObject extends DrawableShape implements IMovable, IRotable, I
         this.speed = speed;
     }
 
+    private void setDirty() {
+        this.dirty = true;
+    }
+
     @Override
     public void setTexture(Texture texture) {
         super.setTexture(texture);
         this.sprite = new Sprite(texture);  // TODO: pass the old sprite parameters (position, rotation, etc.) to the new sprite
         this.sprite.setOriginCenter();
+
+        this.setDirty();
     }
 
     @Override
@@ -43,19 +51,28 @@ public class SpriteObject extends DrawableShape implements IMovable, IRotable, I
 
         this.sprite.setBounds(this.sprite.getX(), this.sprite.getY(), (float) dimension.getWidth(), (float) dimension.getHeight());
         this.sprite.setOrigin((float) dimension.getWidth() / 2.0f, (float) dimension.getHeight() / 2.0f);
+
+        this.setDirty();
     }
 
     @Override
     public void draw(Batch batch) {
-        batch.draw(this.sprite, this.sprite.getX(), this.sprite.getY(),
-                   this.sprite.getOriginX(), this.sprite.getOriginX(),
-                   this.sprite.getWidth(), this.sprite.getHeight(),
-                   this.sprite.getScaleX(), this.sprite.getScaleY(), this.sprite.getRotation());
+        if (this.dirty) {
+            batch.draw(
+                    this.sprite, this.sprite.getX(), this.sprite.getY(),
+                    this.sprite.getOriginX(), this.sprite.getOriginX(),
+                    this.sprite.getWidth(), this.sprite.getHeight(),
+                    this.sprite.getScaleX(), this.sprite.getScaleY(), this.sprite.getRotation());
+
+            this.dirty = false;
+        }
     }
 
     @Override
     public void move() {
         this.setPosition(this.getPosition().add(this.direction.scl(this.speed * Gdx.graphics.getDeltaTime())));
+
+        this.setDirty();
     }
 
     @Override
@@ -67,12 +84,16 @@ public class SpriteObject extends DrawableShape implements IMovable, IRotable, I
     public void setPosition(Vector2 position) {
         super.setPosition(position);
         this.sprite.setPosition(position.x, position.y);
+
+        this.setDirty();
     }
 
     @Override
     public void rotate(float angle) {
         this.direction.setAngle(this.direction.angle() + angle);
         this.sprite.setRotation(this.direction.angle());
+
+        this.setDirty();
     }
 
     @Override
