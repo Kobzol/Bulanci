8fa8d056fa4d01847f8c1ecaa567d2edbc230aa2
Jakub Beránek <berykubik@gmail.com>
2014-11-06 13:51:17 +0100

    Přidána metoda rotateObject.

--- core/src/cz/kobzol/bulanci/player/Player.java
+++ core/src/cz/kobzol/bulanci/player/Player.java
@@ -3,6 +3,7 @@ package cz.kobzol.bulanci.player;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import cz.kobzol.bulanci.model.IDrawable;
 import cz.kobzol.bulanci.model.IGameObject;
+import cz.kobzol.bulanci.model.SpriteObject;
 
 /**
  * Represents player who controlls a game object.
@@ -10,7 +11,7 @@ import cz.kobzol.bulanci.model.IGameObject;
 public class Player implements IDrawable {
     private final int id;
     private final String name;
-    private IGameObject controlledObject;
+    private SpriteObject controlledObject;
 
     public Player(int id, String name) {
         this.id = id;
@@ -19,7 +20,7 @@ public class Player implements IDrawable {
 
     @Override
     public void draw(Batch batch) {
-        ((IDrawable) this.controlledObject).draw(batch);
+        this.controlledObject.draw(batch);
     }
 
     public int getId() {
@@ -34,6 +35,20 @@ public class Player implements IDrawable {
         return this.controlledObject;
     }
     public void setControlledObject(IGameObject controlledObject) {
-        this.controlledObject = controlledObject;
+        this.controlledObject = (SpriteObject) controlledObject;
+    }
+
+    /**
+     * Rotates the player's object.
+     * @param rotateRight true if the rotation is to the right, false if the rotation is to the left
+     */
+    public void rotateObject(boolean rotateRight) {
+        float rotation = 5.0f;
+
+        if (rotateRight) {
+            rotation *= -1.0f;
+        }
+
+        this.controlledObject.rotate(rotation);
     }
 }
