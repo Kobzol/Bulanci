e7647f603727f66c934c8e9cfa9504cd7a585249
Martin Sadovy <sodae@email.cz>
2014-11-08 21:39:19 +0100

    Build server as module

--- server/src/cz/kobzol/bulanci/server/ServerLauncher.java
+++ server/src/cz/kobzol/bulanci/server/ServerLauncher.java
@@ -2,7 +2,7 @@ package cz.kobzol.bulanci.server;
 
 public class ServerLauncher {
     public static void main(String[] args) {
-        BulanciServer server = new BulanciServer(1338, 1338);
+        BulanciServer server = new BulanciServer(16543, 16543);
         server.start();
 
         System.out.println("start server");
