6b3927cc959a35b7522849d90732f7a0d7bf08ee
Martin Sadovy <sodae@email.cz>
2014-11-10 18:09:22 +0100

    Remove redundant method (due to inheritance)

--- core/src/cz/kobzol/bulanci/command/ISignatureCommand.java
+++ core/src/cz/kobzol/bulanci/command/ISignatureCommand.java
@@ -2,9 +2,6 @@ package cz.kobzol.bulanci.command;
 
 import cz.kobzol.bulanci.connection.IIdentifiableMessage;
 
-import java.io.Serializable;
+public interface ISignatureCommand extends IIdentifiableMessage {
 
-public interface ISignatureCommand extends Serializable, IIdentifiableMessage {
-    public int getClientId();
-    public void setClientId(int id);
 }
--- core/src/cz/kobzol/bulanci/connection/IIdentifiableMessage.java
+++ core/src/cz/kobzol/bulanci/connection/IIdentifiableMessage.java
@@ -4,6 +4,6 @@ package cz.kobzol.bulanci.connection;
  * Represents message with sender ID.
  */
 public interface IIdentifiableMessage {
-    void setClientId(int clientId);
-    int getClientId();
+    public void setClientId(int clientId);
+    public int getClientId();
 }
